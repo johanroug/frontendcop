8cfe1e21a843e744dbd8365a1361bed53abe4510
Johan Steiner Roug <johst@orsted.dk>
2020-03-13 13:39:27 +0100

    added components

--- todo/src/app/app.module.ts
+++ todo/src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './components/list/list.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BottomComponent } from './components/bottom/bottom.component';
+import { ItemComponent } from './components/item/item.component';
+import { ItemDetailsComponent } from './components/item/item-details/item-details.component';
+import { SearchComponent } from './components/search/search.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+    ListComponent,
+    HeaderComponent,
+    BottomComponent,
+    ItemComponent,
+    ItemDetailsComponent,
+    SearchComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
