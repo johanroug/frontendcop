4bcc359243bbe259135cb41069526b90239225e0
Johan Steiner Roug <johst@orsted.dk>
2020-03-24 11:16:00 +0100

    no message

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Hello world';
+export class AppComponent implements OnInit {
+  list = [
+    {
+      "id": 1,
+      "active": true,
+      "name": "Tomato"
+    },
+    {
+      "id": 2,
+      "active": false,
+      "name": "Burger"
+    }
+  ];
+  activeItems = [];
+  inActiveItems = [];
+
+  ngOnInit() {
+    this.activeItems = this.filterList(true);
+    this.inActiveItems = this.filterList(false);
+  }
+
+  filterList(activeState) {
+    return this.list.filter(items => {
+      return items.active === activeState;
+    });
+  }
 }
--- todo/src/app/components/list/list.component.ts
+++ todo/src/app/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 
 @Component({
@@ -7,5 +7,10 @@ import {Component} from '@angular/core';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent {
+  @Input() list = [];
 
+  onClickHandler(id) {
+    console.log(id);
+  }
 }
+
